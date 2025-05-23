d8d6dee4d483e72fd13c8b9d5ff39a5c59aa6930
Aayush <amaharjann01@gmail.com>
2025-05-22 20:36:24 -0700

    testing the installed libraries in this file

---
+++ task1_setup/setup_test.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+import sklearn
+import pygame 
+import matplotlib
+
+print(f"Pandas version: {pd.__version__}")
+print(f"Sklearn version: {sklearn.__version__}")
+print(f"Pygame version: {pygame.version.ver}")
+print(f"Matplotlib version: {matplotlib._version}")
